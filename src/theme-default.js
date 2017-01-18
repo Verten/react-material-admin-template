7ae25891afcfe445dc70d80b6996f953d076b80a
ebinhon <bin.hong@ericsson.com>
2017-01-18 17:54:18 +0800

    change css

--- src/theme-default.js
+++ src/theme-default.js
@@ -1,19 +1,31 @@
+import { fade } from 'material-ui/utils/colorManipulator';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import {blue600, grey900} from 'material-ui/styles/colors';
+import * as  Colors from 'material-ui/styles/colors';
 
 const themeDefault = getMuiTheme({
   palette: {
+    primary1Color: Colors.blue600,
+    primary2Color: Colors.cyan700,
+    primary3Color: Colors.lightBlack,
+    accent1Color: Colors.pinkA200,
+    accent2Color: Colors.grey100,
+    accent3Color: Colors.grey500,
+    textColor: Colors.darkBlack,
+    alternateTextColor: Colors.white,
+    canvasColor: Colors.white,
+    borderColor: Colors.grey300,
+    disabledColor: fade(Colors.darkBlack, 0.3)
   },
   appBar: {
     height: 57,
-    color: blue600
+    color: Colors.blue600
   },
   drawer: {
     width: 230,
-    color: grey900
+    color: Colors.grey900
   },
   raisedButton: {
-    primaryColor: blue600,
+    primaryColor: Colors.blue600,
   }
 });
 
